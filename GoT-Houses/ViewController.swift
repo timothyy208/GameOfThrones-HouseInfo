a32d2d35bcecb891de2adacedef9193f834deadf
yangtk <timothyy208@gmail.com>
2019-04-08 16:37:23 -0400

    Initial Commit

---
+++ GoT-Houses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoT-Houses
+//
+//  Created by Timothy Yang on 4/8/19.
+//  Copyright © 2019 Timothy Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
